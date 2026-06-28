2b4c64644c01e13ecf652816a0b3f37b51b4927b
edenCharis <edenngouanda@gmail.com>
2026-06-28 09:46:36 +0100

    Update .gitignore to include .env file; change PDO persistent connection setting to false for database

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ class Database {
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
-                PDO::ATTR_PERSISTENT         => true,
+                PDO::ATTR_PERSISTENT         => false,
                 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->charset
             ];
 
